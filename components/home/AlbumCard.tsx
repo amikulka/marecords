54fdd3f211c4822559a0333f8fc59d300dde91e0
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-01-30 18:23:23 -0700

    removes extra

--- components/home/AlbumCard.tsx
+++ components/home/AlbumCard.tsx
@@ -26,12 +26,6 @@ export default function AlbumCard({ album }: Props) {
             <p>Tracks: {album.track_count}</p>
             <p>Records: {album.disk_count}</p>
           </div>
-          {/* <button
-            type="button"
-            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Button
-          </button> */}
         </div>
       </div>
     </div>
